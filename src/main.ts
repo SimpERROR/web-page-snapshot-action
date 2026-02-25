5cc709aa46278b709825ff3c2c9e76cdbf4e1535
ruirui617 <3067668009@qq.com>
2026-02-25 22:29:17 +0800

    build: 更新依赖项以使用  puppeteer-core

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import * as puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer-core';
 import * as fs from 'fs';
 import * as path from 'path';
 
